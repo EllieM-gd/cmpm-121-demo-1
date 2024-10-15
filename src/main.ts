bd5c89709b290b14653cbc1817089f3177697a6d
Ellie <151094173+EllieM-gd@users.noreply.github.com>
2024-10-15 16:01:26 -0700

    Fixing compile error

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,12 @@ document.title = gameName;
 //Declare number for counter
 let counter: number = 0;
 
-function handleEvent(event) {
-    if (event.type === "click") {
-      counter += 1
-      updateText()
-    }
+function handleEvent(event: Event) {
+  if (event.type === "click") {
+    counter += 1;
+    updateText();
   }
-
+}
 
 //Header
 const header = document.createElement("h1");
@@ -28,18 +27,17 @@ button.addEventListener("click", handleEvent);
 app.appendChild(button);
 
 //Run Once On Run
-updateText()
+updateText();
 //Function to update the text on the screen.
 function updateText() {
-    //Get any existing version of this text
-    const existingDiv = app.querySelector("div");
-    //Delete if it exists
-    if (existingDiv){
-        app.removeChild(existingDiv);
-    }
-    const newDiv = document.createElement("div");
-    const newContent = document.createTextNode(`${counter} Fish! 🎣`);
-    newDiv.appendChild(newContent);
-    app.append(newDiv);
+  //Get any existing version of this text
+  const existingDiv = app.querySelector("div");
+  //Delete if it exists
+  if (existingDiv) {
+    app.removeChild(existingDiv);
+  }
+  const newDiv = document.createElement("div");
+  const newContent = document.createTextNode(`${counter} Fish! 🎣`);
+  newDiv.appendChild(newContent);
+  app.append(newDiv);
 }
-
