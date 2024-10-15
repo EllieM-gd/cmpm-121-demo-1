6153390ded1fa9b2b1c856ea1020cab73ce0af4d
Ellie <151094173+EllieM-gd@users.noreply.github.com>
2024-10-15 15:58:41 -0700

    Step 2

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,41 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "My empty game";
 document.title = gameName;
 
+//Declare number for counter
+let counter: number = 0;
+
+function handleEvent(event) {
+    if (event.type === "click") {
+      counter += 1
+      updateText()
+    }
+  }
+
+
+//Header
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const button = document.createElement('button');
-button.textContent = '🐟';
+//Button
+const button = document.createElement("button");
+button.textContent = "🐟";
+button.addEventListener("click", handleEvent);
 app.appendChild(button);
+
+//Run Once On Run
+updateText()
+//Function to update the text on the screen.
+function updateText() {
+    //Get any existing version of this text
+    const existingDiv = app.querySelector("div");
+    //Delete if it exists
+    if (existingDiv){
+        app.removeChild(existingDiv);
+    }
+    const newDiv = document.createElement("div");
+    const newContent = document.createTextNode(`${counter} Fish! 🎣`);
+    newDiv.appendChild(newContent);
+    app.append(newDiv);
+}
+
